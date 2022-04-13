5f7faadcd7d87c1e150a611213225f2a7cd0c7d4
포뭉 <cherryichigo@naver.com>
2022-04-13 00:35:38 +0000

    자바 파일

---
+++ 30309/HelloEx002.java
@@ -0,0 +1,7 @@
+public class HelloEx002 {
+    public static void main (String[] args) {
+        System.out.print(100);
+        System.out.printIn(100);
+        System.out.printIn("합격점수: " +100);
+    }
+}
